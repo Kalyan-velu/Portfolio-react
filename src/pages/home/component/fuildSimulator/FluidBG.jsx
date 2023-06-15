54eacb680f4ee58f51c2cfb5d8d6b6c8a010a06d
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-06-15 16:47:49 +0530

    "[Added]:Skills Section"

--- src/pages/home/component/fuildSimulator/FluidBG.jsx
+++ src/pages/home/component/fuildSimulator/FluidBG.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useRef } from 'react';
 
-function MyComponent() {
+function FluidBG() {
   const name = useRef(null);
 
   useEffect(() => {
-    const namePaths = name.current.querySelectorAll("#name path");
-    for (let i = 0; i < namePaths.length; i++) {
-      namePaths[i].style.setProperty(
-        "--total-length",
-        namePaths[i].getTotalLength()
-      );
-      namePaths[i].style.setProperty("--anim-delay", `${i * 200}ms`);
-      namePaths[i].addEventListener("mouseenter", () => {
-        namePaths[i].style.transform = `rotate(${
+    const namePaths = Array.from(name.current.querySelectorAll("#name path"));
+
+    namePaths.forEach((path, i) => {
+      const totalLength = path.getTotalLength();
+      path.style.setProperty("--total-length", totalLength);
+      path.style.setProperty("--anim-delay", `${i * 200}ms`);
+    
+      const handleMouseEnter = () => {
+        path.style.transform = `rotate(${
           Math.random() * 2 * (Math.random() < 0.5 ? -1 : 1)
         }deg)`;
-      });
-      namePaths[i].addEventListener("mouseleave", () => {
-        namePaths[i].style.transform = "rotate(0deg)";
-      });
-    }
+      };
+    
+      const handleMouseLeave = () => {
+        path.style.transform = "rotate(0deg)";
+      };
+    
+      path.addEventListener("mouseenter", handleMouseEnter);
+      path.addEventListener("mouseleave", handleMouseLeave);
+    });
+
   }, []);
 
   return (
